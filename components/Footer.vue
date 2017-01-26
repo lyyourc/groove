9e8fb18319340b7e14d8a4f6becabff0f6963a71
Drake Leung <drakeleung42@gmail.com>
2017-01-27 01:18:49 +0800

    :wrench: Init with `nuxt`

---
+++ components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer>
+    Visit source code on <a href="" target="_blank"> GitHub </a>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  padding: 20px;
+  text-align: center;
+  border-top: 1px solid #ddd;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <section class="container">
+    <header>
+      <nuxt-link to="/">
+        <img src="../assets/img/logo.png" alt="logo" class="logo" />
+      </nuxt-link>
+      <nuxt-link to="/">
+        <h1 class="title"> I 🍴 algorithm </h1>
+      </nuxt-link>
+    </header>
+  </section>
+</template>
+
+<style scoped>
+header {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: .6rem 1rem;
+}
+
+.logo {
+  width: 3rem;
+  height: auto;
+  margin-right: 1rem;
+}
+
+.title {
+  color: #505153;
+  font-size: 1.5rem;
+}
+</style>
